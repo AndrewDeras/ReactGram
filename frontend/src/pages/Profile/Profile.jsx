c4f4b01a2b0fa896fd29f3c7c58186db9a6f406c
andrewDeras <andrewderas7@gmail.com>
2023-03-23 15:02:47 -0300

    implementando função de excluir determinada foto. e trazer todas as fotos.

--- frontend/src/pages/Profile/Profile.jsx
+++ frontend/src/pages/Profile/Profile.jsx
@@ -13,7 +13,7 @@ import { useSelector, useDispatch } from "react-redux";
 
 //redux
 import { getUserDetails } from "../../slices/userSlice";
-import { publishPhoto, resetMessage } from "../../slices/photoSlice";
+import { publishPhoto, resetMessage, getUserPhotos, deletePhoto } from "../../slices/photoSlice";
 
 const Profile = () => {
   const { id } = useParams();
@@ -38,16 +38,19 @@ const Profile = () => {
   //load user data
   useEffect(() => {
     dispatch(getUserDetails(id));
+    dispatch(getUserPhotos(id));
   }, [dispatch, id]);
-
-  if (loading) {
-    return <p>Carregando...</p>
-  };
 
   const handleFile = (e) => {
     const image = e.target.files[0];
 
     setImage(image);
+  };
+
+  const resetComponentMessage = () => {
+    setTimeout(() => {
+      dispatch(resetMessage());
+    }, 2000)
   };
 
   const handleSubmit = async (e) => {
@@ -57,7 +60,6 @@ const Profile = () => {
       title,
       image
     };
-
     //build form data
 
     const formData = new FormData();
@@ -69,12 +71,19 @@ const Profile = () => {
 
     setTitle("");
 
-    setTimeout(() => {
-      dispatch(resetMessage());
-    }, 2000)
-
+    resetComponentMessage();
   };
 
+  //delete a photo
+  const handleDelete = (id) => {
+    dispatch(deletePhoto(id));
+
+    resetComponentMessage();
+  };
+
+  if (loading) {
+    return <p>Carregando...</p>
+  };
 
   return (
     <div id="profile" >
@@ -112,6 +121,33 @@ const Profile = () => {
           {messagePhoto && <Message msg={messagePhoto} type="success" />}
         </>
       )}
+      <div className="user-photos">
+        <h2>Fotos publicadas:</h2>
+        <div className="photos-container">
+          {photos && photos.map((photo) => (
+            <div className="photo" key={photo._id} >
+              {photo.image && (<img
+                src={`${uploads}/photos/${photo.image}`}
+                alt={photo.title} />
+              )}
+              {id === userAuth._id
+                ?
+                (
+                  <div className="actions" >
+                    <Link to={`/photos/${photo._id}`}>
+                      <BsFillEyeFill />
+                    </Link>
+                    <BsPencilFill />
+                    <BsXLg onClick={() => handleDelete(photo._id)} />
+                  </div>
+                )
+                : (<Link to={`/photos/${photo._id}`} className="btn" >Ver</Link>)
+              }
+            </div>
+          ))}
+          {photos.length === 0 && <p>Ainda não há fotos publicadas.</p>}
+        </div>
+      </div>
     </div>
   )
 }
